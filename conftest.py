6d29a27d928278780d3098eaf798de4eefaa7e70
chemodanowaleksandr-blip <chemodanow.aleksandr@gmail.com>
2026-07-26 19:42:10 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -5,11 +5,13 @@ from selenium import webdriver
 def driver(request):
     if request.param == "chrome":
         options = webdriver.ChromeOptions()
-        # options.add_argument("--headless")
+        options.add_argument("--headless")  # Запуск без графического экрана
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
         driver = webdriver.Chrome(options=options)
     elif request.param == "firefox":
         options = webdriver.FirefoxOptions()
-        # options.add_argument("--headless")
+        options.add_argument("--headless")  # Запуск без графического экрана
         driver = webdriver.Firefox(options=options)
     
     driver.set_window_size(1920, 1080)
