babb4a5adb2c248e995d96054003cb339b61aa43
chemodanowaleksandr-blip <chemodanow.aleksandr@gmail.com>
2026-07-26 18:53:10 +0300

    Create base_page.py

---
+++ pages/base_page.py
@@ -0,0 +1,23 @@
+import allure
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class BasePage:
+    def __init__(self, driver):
+        self.driver = driver
+
+    def find_element_with_wait(self, locator, timeout=10):
+        return WebDriverWait(self.driver, timeout).until(
+            EC.presence_of_element_located(locator)
+        )
+
+    def wait_for_visibility(self, locator, timeout=10):
+        return WebDriverWait(self.driver, timeout).until(
+            EC.visibility_of_element_located(locator)
+        )
+
+    def click_element(self, locator):
+        self.find_element_with_wait(locator).click()
+
+    def get_text_from_element(self, locator):
+        return self.find_element_with_wait(locator).text
