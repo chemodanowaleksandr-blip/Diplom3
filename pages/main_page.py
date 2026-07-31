b1e0a43096297d2570117781e6391d1f067baf2c
chemodanowaleksandr-blip <chemodanow.aleksandr@gmail.com>
2026-07-31 12:53:21 +0300

    Update main_page.py

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,28 +1,30 @@
 import allure
 from pages.base_page import BasePage
 from locators.main_page_locators import MainPageLocators
+import data
 
 class MainPage(BasePage):
-    @allure.step("Кликнуть по кнопке 'Конструктор'")
-    def click_constructor_button(self):
-        self.click_element(MainPageLocators.CONSTRUCTOR_BUTTON)
+
+    @allure.step("Открыть главную страницу Stellar Burgers")
+    def open_main_page(self):
+        self.driver.get(data.BASE_URL)
 
     @allure.step("Кликнуть по кнопке 'Лента Заказов'")
     def click_order_feed_button(self):
-        self.click_element(MainPageLocators.ORDER_FEED_BUTTON)
+        self.click_to_element(MainPageLocators.ORDER_FEED_BUTTON)
+
+    @allure.step("Кликнуть по кнопке 'Конструктор'")
+    def click_constructor_button(self):
+        self.click_to_element(MainPageLocators.CONSTRUCTOR_BUTTON)
 
     @allure.step("Кликнуть по первому ингредиенту")
     def click_first_ingredient(self):
-        self.click_element(MainPageLocators.INGREDIENT_CARD)
+        self.click_to_element(MainPageLocators.FIRST_INGREDIENT)
 
-    @allure.step("Проверить видимость модального окна с деталями")
+    @allure.step("Проверить видимость модального окна с деталями ингредиента")
     def is_modal_details_visible(self):
-        return self.wait_for_visibility(MainPageLocators.MODAL_DETAILS).is_displayed()
+        return self.is_element_visible(MainPageLocators.MODAL_DETAILS)
 
-    @allure.step("Закрыть модальное окно кликом по крестику")
+    @allure.step("Закрыть модальное окно кликом на крестик")
     def click_close_modal_button(self):
-        self.click_element(MainPageLocators.MODAL_CLOSE_BUTTON)
-
-    @allure.step("Получить значение счетчика ингредиента")
-    def get_ingredient_counter_value(self):
-        return self.get_text_from_element(MainPageLocators.INGREDIENT_COUNTER)
+        self.click_to_element(MainPageLocators.CLOSE_MODAL_BUTTON)
